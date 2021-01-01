b678eefa9e135c8930063edc7a1bd7ee134b7d28
Aleksey <tarnado_0993@mail.ru>
2021-01-01 21:33:41 +0300

    create project

---
+++ src/main/java/com/institute/tagan/diaryinstitute/DiaryInstituteApplication.java
@@ -0,0 +1,13 @@
+package com.institute.tagan.diaryinstitute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiaryInstituteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiaryInstituteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/institute/tagan/diaryinstitute/DiaryInstituteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.institute.tagan.diaryinstitute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiaryInstituteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
